2a5d4f607ab1038d0e3d9df5eb86fd2737278414
Jake Moore <jake.moorevr@gmail.com>
2018-04-07 21:21:15 -0300

    Created FPS Controller
    Scene now has an FPS controller, with wasd movement and mouse to look.

---
+++ horrorgame/Assets/Scripts/fpsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsController : MonoBehaviour {
+
+	public float speed = 2f;
+	public float sensitivity = 2f;
+	public GameObject camera;
+	CharacterController player;
+
+	//Keyboard movement
+	float moveFB;
+	float moveLR;
+
+	//Mouse movement
+	float rotX;
+	float rotY;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GetComponent<CharacterController> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Creates movement for player with wasd
+		moveFB = Input.GetAxis ("Vertical") * speed;
+		moveLR = Input.GetAxis ("Horizontal") * speed;
+
+		Vector3 movement = new Vector3 (moveLR, 0,moveFB);
+		movement = transform.rotation * movement;
+		player.Move (movement * Time.deltaTime);
+
+		//Mouse looking
+		rotX = Input.GetAxis("Mouse X") * sensitivity;
+		rotY = Input.GetAxis("Mouse Y") * sensitivity;
+		transform.Rotate (0, rotX, 0);
+		camera.transform.Rotate (-rotY, 0, 0);
+
+
+	}
+}
