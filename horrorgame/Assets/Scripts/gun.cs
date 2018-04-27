26fff55002ef463f5515fa56eb24c7c8a8581782
Jake Moore <jake.moorevr@gmail.com>
2018-04-27 20:13:06 -0300

    Added gun
    Gun shoots with muzzle flash, impact smoke, and can move objects with force.  Target script controls health of an enemy, and object is destroyed once health is 0.

---
+++ horrorgame/Assets/Scripts/gun.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 100f;
+
+	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+	public GameObject impactEffect;
+	public float impactForce = 60f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1")) {
+			shoot ();
+		}
+
+	}
+
+	void shoot () {
+
+		muzzleFlash.Play ();
+
+		RaycastHit hit;
+		if (Physics.Raycast (fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+			Debug.Log (hit.transform.name);
+
+			target target = hit.transform.GetComponent<target> ();
+
+			if (target != null) {
+				target.TakeDamage (damage);
+			}
+
+			if (hit.rigidbody != null) {
+				hit.rigidbody.AddForce (-hit.normal * impactForce);
+			}
+
+			GameObject impactGO = Instantiate (impactEffect, hit.point, Quaternion.LookRotation (hit.normal));
+			Destroy (impactGO, 2f);
+		}
+	}
+}
